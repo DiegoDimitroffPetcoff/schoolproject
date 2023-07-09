ae28f06ceac4c6c0733a7d0746d74b618f3d20da
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-07-09 22:56:48 +0000

    Add buttons and Navbar

--- src/containers/homePageContainer.js
+++ src/containers/homePageContainer.js
@@ -1,11 +1,19 @@
 import Logo from "../components/pure/Logo";
+import Navbar from "../components/pure/Navbar";
 import Bottom from "../components/pure/Buttom";
 import Alert from "../utils/alertTest";
+import "../styles/homePageStyles.css";
 function HomePage() {
   return (
-    <div>
-      <Logo></Logo> <Bottom string="Registrarse" action={Alert}></Bottom>
-      Home Page
+    <div className="homePageContainer">
+      <div className="head">
+      <Logo></Logo> 
+      <div className="bottomContainer"><Bottom string="Registrarse" action={Alert}></Bottom></div>
+      <Navbar></Navbar>
+      </div>
+      <div className="body">body</div>
+      <div className="footer">footer</div>
+      <div className="body">footer12</div>
     </div>
   );
 }
