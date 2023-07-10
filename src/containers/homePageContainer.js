d83b31f5345e6f1d17510474cd63f1abd8cc7964
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-07-10 02:01:59 +0000

    Trying Carrousel

--- src/containers/homePageContainer.js
+++ src/containers/homePageContainer.js
@@ -2,6 +2,7 @@ import Logo from "../components/pure/Logo";
 import Navbar from "../components/pure/Navbar";
 import Bottom from "../components/pure/Buttom";
 import Alert from "../utils/alertTest";
+import Carrousel from "../components/pure/Carrousel";
 import "../styles/homePageStyles.css";
 function HomePage() {
   return (
@@ -11,7 +12,11 @@ function HomePage() {
       <div className="bottomContainer"><Bottom string="Registrarse" action={Alert}></Bottom></div>
       <Navbar></Navbar>
       </div>
-      <div className="body">body</div>
+
+      <div className="body">
+        <Carrousel></Carrousel>
+        </div>
+
       <div className="footer">footer</div>
       <div className="body">footer12</div>
     </div>
