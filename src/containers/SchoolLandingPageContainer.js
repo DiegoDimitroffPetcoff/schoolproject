174024e5099880fe53604d3b6793a7185b0b98f1
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-07-16 23:45:13 +0000

    FIX some text

--- src/containers/SchoolLandingPageContainer.js
+++ src/containers/SchoolLandingPageContainer.js
@@ -5,7 +5,7 @@ function SchoolLandingPage() {
     return(
         <div className="SectionContainer">
         <div className="BorderContainer">
-            <h1 className="titleTwo">ESCUELA PEDICURA Y MANICURA</h1>
+            <h1 className="titleTwo">TALLERES PEDICURA Y MANICURA</h1>
             <p className="subTitleTwo">Nuestro trabajo es el de hacer y enseñar.. aprender y compartir</p>
            <p className="textTwo"> Sumate a aprender algo nuevo, divertido y rentable! </p>
                       </div>
--- src/containers/whoWeAreContainer.js
+++ src/containers/whoWeAreContainer.js
@@ -8,7 +8,7 @@ function WhoWeAre() {
             <p className="subTitle">Nuestro trabajo es el de hacer y enseñar.. aprender y compartir</p>
             <p className="text">Mi nombre es Vanina Zucarelli, trabajando y dirigiendo es hermoso emprendimiento desde hace mas de 10 años, quiero darte la bienvenida, espero puedas encontrar este sitio interesante, en donde podras ver los trabajos realizados tanto por mi como por mis alumnos</p>
             <p className="text">Animate a formar parte de este grupo! </p>
-            <p className="text"><Buttom2 string="UNETE A NUESTRA ESCUELA!"></Buttom2></p>
+            <p className="text"><Buttom2 string="UNETE A NUESTROS TALLERES!"></Buttom2></p>
             
         </div>
     )
