3f4ba160f3e3b7c6a4e3fdfd23dc4e880968481d
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2023-07-17 09:00:59 +0100

    ADD x to close menu responsive

--- src/components/pure/NavbarBurguer.js
+++ src/components/pure/NavbarBurguer.js
@@ -1,57 +1,58 @@
-
-import React, { useState } from 'react';
-import Buttom from './Buttom';
-import '../../styles/navbar/navbar.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import Buttom from "./Buttom";
+import Buttom2 from "./Buttom2";
+import "../../styles/navbar/navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const NavbarBurger = () => {
   const [isOpen, setIsOpen] = useState(true);
 
   function openBar() {
-    setIsOpen( !isOpen) 
+    setIsOpen(!isOpen);
   }
   return (
     <>
-      
- {isOpen     ?
- (<button onClick={openBar} className='NavbarBurguerIcon'>   <FontAwesomeIcon icon={faBars} style={{ color: '#f2ced8' }} className="fa-4x" /> </button>) :
-
-
- (    
- <nav className='NavbarBurguer' >
-  <ul>
-  <li >
-   <Buttom string="INICIO"></Buttom>
-        </li>
-        <li>
-        <Buttom string="INGRESAR"></Buttom>
-        </li>
-        <li>
-        <Buttom string="REGISTRARTE"></Buttom>
-        </li>
-        <li>
-        <Buttom string="QUIENES SOMOS"></Buttom>
-        </li>
-        <li>
-        <Buttom string="AGENDA TU CITA"></Buttom>
-        </li>
-        <li>
-        <Buttom string="DISEÑOS"></Buttom>
-        </li>
-        <li>
-        <Buttom string="ESCUELA"></Buttom>
-        </li>
- </ul>
-</nav>)
-
-}    
-      
-
- 
+      {isOpen ? (
+        <button onClick={openBar} className="NavbarBurguerIcon">
+          {" "}
+          <FontAwesomeIcon
+            icon={faBars}
+            style={{ color: "#f2ced8" }}
+            className="fa-4x"
+          />{" "}
+        </button>
+      ) : (
+        <nav className="NavbarBurguer">
+        
+          <ul>
+          <Buttom2 string="X" action={()=>{setIsOpen(!isOpen);}}></Buttom2>
+            <li>
+              <Buttom string="INICIO"></Buttom>
+            </li>
+            <li>
+              <Buttom string="INGRESAR"></Buttom>
+            </li>
+            <li>
+              <Buttom string="REGISTRARTE"></Buttom>
+            </li>
+            <li>
+              <Buttom string="QUIENES SOMOS"></Buttom>
+            </li>
+            <li>
+              <Buttom string="AGENDA TU CITA"></Buttom>
+            </li>
+            <li>
+              <Buttom string="DISEÑOS"></Buttom>
+            </li>
+            <li>
+              <Buttom string="ESCUELA"></Buttom>
+            </li>
+          </ul>
+        </nav>
+      )}
     </>
   );
 };
 
 export default NavbarBurger;
-
