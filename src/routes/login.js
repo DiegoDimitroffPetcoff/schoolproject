f70519246545bb9e9ff4e8a3edbc6f3f32c65c9f
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2023-07-17 09:05:59 +0100

    Add routes files, login form

---
+++ src/routes/login.js
@@ -0,0 +1,10 @@
+import LoginForm from "../components/pure/forms/login"
+
+function Login(params) {
+    return(
+        <div>
+            <LoginForm></LoginForm>
+        </div>
+    )
+}
+export default Login
