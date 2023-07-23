72e6e2f502a12196cf56f6cafb5fb0eb4c36c3f9
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-07-23 22:56:22 +0000

    ADD content on form

--- src/components/forms/register.js
+++ src/components/forms/register.js
@@ -6,40 +6,148 @@ import "../../styles/form/formContainer.css";
 import Buttom1 from "../pure/Buttom2";
 
 function RegisterForm() {
+  const [firstName, setFirstName] = useState("");
+  const [secondName, setSecondName] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [nickName, setNickName] = useState("");
+  const [password, setPassword] = useState("");
+  const [document, setDocument] = useState("");
+  const [cellPhone, setCellPhone] = useState("");
   const [email, setEmail] = useState("");
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleSecondNameChange = (event) => {
+    setSecondName(event.target.value);
+  };
+  const handleDateOfBirthChange = (event) => {
+    setDateOfBirth(event.target.value);
+  };
+  const handleNickNameChange = (event) => {
+    setNickName(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleDocumentChange = (event) => {
+    setDocument(event.target.value);
+  };
+  const handleCellPhoneChange = (event) => {
+    setCellPhone(event.target.value);
+  };
+
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
+
+
   };
 
   const handleSubmit = (event) => {
     event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
-    console.log(`Email: ${email}`);
+    const formData = {
+      name:{
+        firstName: firstName,
+        secondName: secondName,
+        dateOfBirth: dateOfBirth,      
+      },
+  
+      nickname: nickName,
+      password: password,
+      document: {
+        number:document,
+        typeOf: "DNI"      
+      },
+      contact:{
+        cellPhone: cellPhone,
+        email: email
+      },
+      avatar:"avatar"
+    };
+
+    // Convertir el objeto a una cadena JSON y mostrarlo en la consola
+    const formDataJSON = JSON.stringify(formData);
+    console.log(formDataJSON);
   };
 
   return (
-    <form className="formContainer">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+ 
+      <Form className="formContainer" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="firstName">
           <Form.Control
             type="text"
-            placeholder="Nombre Completo"
-            onChange={handleEmailChange}
+            placeholder="Nombre"
+            onChange={handleFirstNameChange}
+            value={firstName}
+            required
           />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3" controlId="secondName">
           <Form.Control
             type="text"
             placeholder="Apellido"
-            onChange={handleEmailChange}
+            onChange={handleSecondNameChange}
+            value={secondName}
+            required
           />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3" controlId="dateOfBirth">
+          <Form.Control
+            type="date"
+            placeholder="Fecha de Nacimiento"
+            onChange={handleDateOfBirthChange}
+            value={dateOfBirth}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="nickName">
+          <Form.Control
+            type="text"
+            placeholder="Usuario"
+            onChange={handleNickNameChange}
+            value={nickName}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Control
+            type="password"
+            placeholder="Contraseña"
+            onChange={handlePasswordChange}
+            value={password}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="document">
+          <Form.Control
+            type="number"
+            placeholder="Documento"
+            onChange={handleDocumentChange}
+            value={document}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="cellPhone">
+          <Form.Control
+            type="phonenumber"
+            placeholder="Telefono"
+            onChange={handleCellPhoneChange}
+            value={cellPhone}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="email">
           <Form.Control
             type="email"
             placeholder="Email"
             onChange={handleEmailChange}
+            value={email}
+            required
           />
         </Form.Group>
 
@@ -55,7 +163,7 @@ function RegisterForm() {
           </Buttom1>
         </div>
       </Form>
-    </form>
+
   );
 }
 
