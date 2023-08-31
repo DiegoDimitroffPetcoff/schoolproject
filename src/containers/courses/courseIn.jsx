c6b87657c82aa666f73f940d7069e5721b47e525
DimiiDiego <125349175+DimiiDiego@users.noreply.github.com>
2023-08-31 23:06:49 +0000

    ADD videos in courses

--- src/containers/courses/courseIn.jsx
+++ src/containers/courses/courseIn.jsx
@@ -1,26 +1,62 @@
 import "../../styles/sectionOne.css";
 import "../../styles/course/courseIn/courseInContainer.css"
 import Cookies from "js-cookie"
+import { byAngle } from "@cloudinary/url-gen/actions/rotate";
+import Buttom from "../../components/pure/Buttom2"
+import { useState } from "react";
+
+
 
 function CourseIn() {
   let courseCookie = Cookies.get("course");
-  let course = JSON.parse(courseCookie)
+  let course = JSON.parse(courseCookie);
+  let [toggleButton, setToggleButon] = useState(false)
 
-  return (    
-  <>
-  <div className="courseInContainer">
-  <h1 className="title"> Taller: {course.name}</h1>
-  <h3 className="subTitle">  Informacion: {course.content}</h3>
-  <img src={course.img}/>
-  </div>
-  <div>
-    <h1>CLASES</h1>
-    VIDEO1
-    VIDEO2
-    VIDEO3
-    VIDEO4
-  </div>
-  </>
-  )
+  let mockVideos = [
+    { video: "https://res.cloudinary.com/demo/video/upload/elephants" },
+    { video: "https://youtube.com/watch?v=DFg1V-rO6Pg&t=4716s" },
+  ];
+
+  const [videoStates, setVideoStates] = useState(
+    mockVideos.map(() => false) // Inicializa todos los estados como falso
+  );
+
+  const handleToggle = (index) => {
+    const updatedStates = [...videoStates]; 
+    updatedStates[index] = !updatedStates[index]; 
+    setVideoStates(updatedStates); 
+    setToggleButon(!toggleButton)
+  };
+
+  return (
+    <>
+      <div className="courseInContainer">
+        <h1 className="title"> Taller: {course.name}</h1>
+        <h3 className="subTitle"> Informacion: {course.content}</h3>
+        <img src={course.img} alt={course.name} />
+      </div>
+
+      
+        {mockVideos.map((video, index) => (
+          <div className="courseInContainer"key={index}>
+            <Buttom
+              string={toggleButton ? ("Cerrar") : ("Ver video")}
+              action={() => handleToggle(index)}
+            />
+            {videoStates[index] && (
+              <video
+                className="courseInContainer"
+                style={{ width: "50%", margin: "0 auto" }}
+                controls
+              >
+                <source src={video.video} type="video/mp4" />
+              </video>
+            )}
+          </div>
+        ))}
+   
+    </>
+  );
 }
+
 export default CourseIn;
